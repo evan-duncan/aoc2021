c3a10f19fe1f4cd565e31e157a35f94c433c8c98
Evan Duncan <evan.duncan@greenhouse.io>
2021-12-08 10:54:55 -0600

    non working day 2 part 2

--- src/aoc2021/day2.clj
+++ src/aoc2021/day2.clj
@@ -3,9 +3,33 @@
 
 (def planned-course (split-lines (slurp "./day2.text")))
 
-(defn directions [s]
-  (let [[k v] (split s #"\s")]
+(defn directions [s] (split s #"\s"))
+
+(defn v->m [v]
+  (let [[k v] v]
     (hash-map k (Integer/parseInt v))))
 
-(let [{:strs [forward up down]} (apply merge-with + (map directions planned-course))]
+;; Part 1
+(let [{:strs [forward up down]} (apply merge-with + (map v->m (map directions planned-course)))]
   (* forward (- down up)))
+
+
+;; Part 2
+(def calculations {:aim 0 :position { :x 0 :y 0 }})
+
+(defn down [calcs x]
+  (update calcs :aim (fn [n] (+ n x))))
+
+(defn up [calcs x]
+  (update calcs :aim (fn [n] (- n x))))
+
+(defn forward [calcs x]
+  (merge-with
+   (update-in calcs [:position :x] + x)
+   (update-in calcs [:position :y] + (* x (get calcs :aim)))))
+
+(reduce
+ (fn [calcs d]
+   (merge-with (resolve (symbol (first d))) calcs (Integer/parseInt (last d))) calcs)
+ calculations
+ (map directions planned-course))
